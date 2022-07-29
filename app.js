a806ac44cd97e1ce5652508b0b0f813e40596121
Amir <amir.akbrzadh3@gmail.com>
2022-07-29 17:40:13 +0430

    fully responsive and add more functionallity

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ range.addEventListener("input" , (e) => {
 const swiper = new Swiper(".swiper", {
      direction: "horizontal",
      slidesPerView: 1,
+     spaceBetween: 10,
      pagination: {
         el: ".swiper-pagination",
      },
@@ -31,6 +32,7 @@ const swiper = new Swiper(".swiper", {
   const swiperpopularpodcast = new Swiper(".swiperpopularpodcast", {
    direction: "horizontal",
    slidesPerView: 1,
+   spaceBetween: 10,
    navigation: {
       nextEl: ".swiper-button-nex",
       prevEl: ".swiper-button-pre",
@@ -46,6 +48,32 @@ const swiper = new Swiper(".swiper", {
       },
    },
 });
-  /*
-  
-   */
+// hamburger menu functionall
+let hamburgrMenu = document.querySelector(".hamburgrmenu") ; 
+let leftTab = document.querySelector(".lefttabb") ; 
+hamburgrMenu.addEventListener("click" , (e) => {
+   hamburgrMenu.classList.toggle("activehamburgrmenu") ; 
+   leftTab.classList.toggle("translate-x-[-100%]")
+   for (let i = 0; i < hamburgrMenu.children[0].children.length ; i++) {
+      if(i === 1) {
+         hamburgrMenu.children[0].children[1].classList.toggle("opacity-0") ; 
+      }else{
+         hamburgrMenu.children[0].children[i].classList.toggle("top-0") ; 
+         hamburgrMenu.children[0].children[i].classList.toggle("top-halfhamburger") ; 
+         hamburgrMenu.children[0].children[i].classList.toggle("translate-y-[-50%]") ; 
+         hamburgrMenu.children[0].children[i].classList.toggle("rotate-[-135deg]") ; 
+      }
+   }
+   hamburgrMenu.children[0].children[2].classList.toggle("rotate-[135deg]") ; 
+})
+let catbutton = document.querySelectorAll(".catbut") ; 
+console.log(catbutton);
+catbutton.forEach((e) => {
+   e.addEventListener("click" , (eve) => {
+      catbutton.forEach((e) => {
+         e.classList.toggle("text-white") ;
+         e.classList.toggle("text-gray-500") ;
+         e.classList.toggle("bg-bgNav") ;
+      })
+   })
+})
